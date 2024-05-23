73670f13a8edb39373651a69876b86e8ebecfd7e
habiburrahman-mu <habiburrahman-mu@users.noreply.github.com>
2024-05-23 21:38:50 +0600

    added rabbitmq producer and consumer projects

---
+++ Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox",
+    durable: false,
+    exclusive: false, autoDelete: false, arguments: null);
+Console.WriteLine(" [*] Waiting for message");
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += OnReceivedMessage;
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+void OnReceivedMessage(object? sender, BasicDeliverEventArgs ea)
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+
+var message = "This is my first Message";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "", routingKey: "letterbox", body: body);
+Console.WriteLine($"Message {message} published.");
