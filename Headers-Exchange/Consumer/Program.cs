97113f3ff6aa3f0c0fd47ec9a58c5c1d4a613dd4
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-06-08 12:41:52 +0600

    exchanges: added example for headers exchange

--- Headers-Exchange/Consumer/Program.cs
+++ Headers-Exchange/Consumer/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "headersExchange", type: ExchangeType.Headers);
+channel.QueueDeclare(queue: "letterbox");
+var bindingArguments = new Dictionary<string, object>
+{
+    {"x-match", "any"},
+    {"name", "test"},
+    {"age", "21"},
+};
+channel.QueueBind("letterbox", "headersExchange", "", bindingArguments);
+
+Console.WriteLine(" [*] Waiting for message");
+var consumer = new EventingBasicConsumer(channel);
+
+var random = new Random();
+
+consumer.Received += OnReceivedMessage;
+
+channel.BasicConsume(queue: "letterbox", autoAck: false, consumer: consumer);
+Console.WriteLine("Consuming");
+Console.ReadKey();
+
+
+void OnReceivedMessage(object? sender, BasicDeliverEventArgs ea)
+{
+    var processingTime = random.Next(1, 6);
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+}
